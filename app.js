b82028d287a3a0a0bd800ba70f1d350821e26bdb
Vinicius Ibanez <vinicius.ibanez97@gmail.com>
2021-05-17 14:38:55 -0300

    change root folder

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-process.env.PWD = process.cwd();
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static('public/assets'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
