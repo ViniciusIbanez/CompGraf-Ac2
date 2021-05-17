5b00a5181ba804cb85adebe37ea94829827bbd69
Vinicius Ibanez <vinicius.ibanez97@gmail.com>
2021-05-17 14:47:04 -0300

    change static

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static('public/assets'));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
