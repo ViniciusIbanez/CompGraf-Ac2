79a5148f890e47b7b2298469dc2ebe4d71782983
Vinicius Ibanez <vinicius.ibanez97@gmail.com>
2021-05-17 14:20:43 -0300

    update node static path

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+process.env.PWD = process.cwd();
+app.use(express.static(path.join(process.env.PWD, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
